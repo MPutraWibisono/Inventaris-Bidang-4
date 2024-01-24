4e5846e84bb2617c8b454484fbaa96a8c685b2fe
nadifapurnomo <purnomodifa@gmail.com>
2024-01-24 16:46:00 +0800

    role user

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BarangKeluarController;
 use App\Http\Controllers\BarangMasukController;
+use GuzzleHttp\Middleware;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,6 +21,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('/barangkeluar', BarangKeluarController::class);
-Route::resource('/barangmasuk', BarangMasukController::class);
-Route::post('/barangmasuk/export', [BarangMasukController::class, 'export'])->name('barangmasuk.export');
+Route::middleware('auth')->group(function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::group(['middleware' => ['role:user']], function () {
+        Route::resource('/barangkeluar', BarangKeluarController::class);
+    });
+    Route::group(['middleware' => ['role:admin']], function () {
+        Route::resource('/barangmasuk', BarangMasukController::class);
+        Route::post('/barangmasuk/export', [BarangMasukController::class, 'export'])->name('barangmasuk.export');
+    });
+});
+
+Auth::routes();
