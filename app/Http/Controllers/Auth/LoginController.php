32d0b5d5f5326ba5563fb69bb414285d83551e7d
KustyBoo <christoperafrian@gmail.com>
2024-01-26 10:44:56 +0800

    Menambah login page dan routing

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -20,6 +20,11 @@ class LoginController extends Controller
 
     use AuthenticatesUsers;
 
+    public function index()
+    {
+        return view("login");
+    }
+
     /**
      * Where to redirect users after login.
      *
